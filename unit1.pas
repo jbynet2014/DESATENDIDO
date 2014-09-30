33c19ca0dadebd8242a8685aeade027c926d9844
jbynet2014 <jhons_qm@hotmail.com>
2014-09-30 11:08:23 -0500

    PROYECTO DE INSTALACION DESATENDIDA
    Sirve para que cualquier tipo de instalador lo ejecutes de manera
    desatendida

---
+++ unit1.pas
@@ -0,0 +1,30 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
